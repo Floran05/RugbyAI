6818a47c8352385ea737a6f12b305f3890a6ac81
Floran05 <carvalhofloran@gmail.com>
2025-01-13 14:38:56 +0100

    Add SFML missing files

--- RugbyAI/RugbyScene.cpp
+++ RugbyAI/RugbyScene.cpp
@@ -1,4 +1,5 @@
 #include "RugbyScene.h"
+#include "Debug.h"
 
 void RugbyScene::OnInitialize()
 {
@@ -10,4 +11,9 @@ void RugbyScene::OnEvent(const sf::Event& event)
 
 void RugbyScene::OnUpdate()
 {
+	// Draw goal lines
+	int windowWidth = GetWindowWidth();
+	int windowHeight = GetWindowHeight();
+	Debug::DrawLine(windowWidth * 0.1f, 0, windowWidth * 0.1f, windowHeight, sf::Color::White);
+	Debug::DrawLine(windowWidth * 0.9f, 0, windowWidth * 0.9f, windowHeight, sf::Color::White);
 }
