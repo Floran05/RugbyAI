d3bd83d4e392863da3b857b6aa68c1cc194f7096
l3ith <75183836+l3ith@users.noreply.github.com>
2025-01-14 10:22:48 +0100

    update 14/01 -01- Player bounding box

--- RugbyAI/RugbyScene.cpp
+++ RugbyAI/RugbyScene.cpp
@@ -1,10 +1,19 @@
 #include "RugbyScene.h"
 #include "Debug.h"
-
+#include "Player.h"
 #include <iostream>
-
+#define ZONE_COUNT 3
 void RugbyScene::OnInitialize()
 {
+	int width = GetWindowWidth();
+	int height = GetWindowHeight();
+	float zoneHeight = height / (ZONE_COUNT*2.f - 2);
+	float zoneWidt = width;
+	for (int i = 0; i < ZONE_COUNT; ++i) {
+		int yMin = i * zoneHeight;
+		int yMax = yMin + zoneHeight*2;
+		mAreas[i] = { 0,width,yMin,yMax };
+	}
 }
 
 void RugbyScene::OnEvent(const sf::Event& event)
@@ -18,4 +27,11 @@ void RugbyScene::OnUpdate()
 	int windowHeight = GetWindowHeight();
 	Debug::DrawLine(windowWidth * 0.1f, 0, windowWidth * 0.1f, windowHeight, sf::Color::White);
 	Debug::DrawLine(windowWidth * 0.9f, 0, windowWidth * 0.9f, windowHeight, sf::Color::White);
+	for (int i = 0; i < ZONE_COUNT; i++)
+	{
+		const Box& box = mAreas[i];
+
+	Debug::DrawRectangle(box.xMin, box.yMin, box.width, box.height, mColors[i]);
+	}
+	
 }
--- RugbyAI/RugbyScene.h
+++ RugbyAI/RugbyScene.h
@@ -33,7 +33,8 @@ public:
 		PlayerRed,
 		Ball
 	};
-
+	Box mAreas[3];
+	sf::Color mColors[3] = { sf::Color::Red,sf::Color::Blue,sf::Color::White };
 	void OnInitialize() override;
 	void OnEvent(const sf::Event& event) override;
 	void OnUpdate() override;
